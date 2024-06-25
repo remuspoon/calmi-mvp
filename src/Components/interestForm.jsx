5449cc02444f6e431894e22eeb6d91e24139c9a1
Remus <remuspoonucl@gmail.com>
2024-06-25 14:19:38 +0800

    Interest form css changes

--- src/Components/interestForm.jsx
+++ src/Components/interestForm.jsx
@@ -3,13 +3,18 @@ import React from 'react'
 const InterestForm = () => {
     return (
         <form className='flex flex-col items-center drop-shadow-lg font-baloo-chettan-2'>
-            <h1 className="text-center font-bold text-4xl mt-8">Interest Form</h1>
-            <div className="flex flex-col items-start mt-4"> {/* Add a container div with top margin */}
-                <div className="flex flex-row items-center mb-4"> {/* Add a flex container for each label and input with bottom margin */}
+            <h1 className="text-center font-bold text-4xl mt-8">Join the waitlist!</h1>
+            <div className="flex flex-col items-start mt-4"> 
+                <div className="flex flex-row items-center mb-4"> 
                     <input className="rounded border mx-auto block w-64 p-2 text-black" placeholder="Name" />
                 </div>
-                <div className="flex flex-row items-center mb-4"> {/* Add a flex container for each label and input with bottom margin */}
+                <div className="flex flex-row items-center mb-4">
                     <input className="rounded border mx-auto block w-64 p-2 text-black" placeholder="Email" />
+                </div>
+                <div className="flex flex-row items-center mb-4">
+                    <button className="rounded border mx-auto block w-64 p-2 text-white text-center bg-blue-950 text-MD shadow-lg justify-center font-baloo-chettan-2 font-medium border-blue-950 hover:bg-opacity-0 hover:border-2 hover:border-blue-950 hover:text-blue-950 duration-200">
+                        Submit
+                    </button>
                 </div>
             </div>
         </form>
