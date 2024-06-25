54200a33617d101cb093ccc43d5ab416946a116e
Remus <remuspoonucl@gmail.com>
2024-06-25 13:49:38 +0800

    css changes to interest form

--- src/Components/interestForm.jsx
+++ src/Components/interestForm.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 
 const InterestForm = () => {
     return (
-        <form className='form'>
-            <h1>Interest Form</h1>
-            <label>Name</label>
-            <input placeholder="Name"/>
+        <form className='flex flex-col items-center drop-shadow-lg font-baloo-chettan-2 '>
+            <h1 className="text-center font-bold text-4xl mt-8 ">Interest Form</h1>
+            <label className="text-center font-medium mt-3 mb-2 text-lg">Name</label>
+            <input className="rounded border mx-auto block w-64 p-2 text-black" placeholder="Name" />
 
-            <label>Email</label>
-            <input placeholder="Email"/>
+            <label className="text-center font-medium mt-3 mb-2 text-lg">Email</label>
+            <input className="rounded border mx-auto block w-64 p-2 text-black" placeholder="Email" />
 
-            <label>How do you improve your own mental health?</label>
-            <textarea placeholder="Type Here: "></textarea>
+            <label className="text-center font-medium mt-3 mb-2 text-lg">How do you improve your own mental health?</label>
+            <textarea className="rounded border mx-auto block w-64 p-2 text-black" placeholder="Type Here:" />
         </form>
     )
 }
